1dc52f8645edea3bf06c421a55c82d548d4764f0
kaue-ferreira652 <10kaue981@gmail.com>
2026-04-07 16:06:24 -0300

    subindo ex07.js

---
+++ ex07.js
@@ -0,0 +1,12 @@
+const readline = require("readline-sync")
+
+let nota = 7
+let notaFinal = readline.questionFloat("Informe a sua nota: ")
+
+let = nota - notaFinal
+console.log("====== PROVA DO ALUNO FULANO ======")
+if(notaFinal >= nota){
+    console.log("Parabéns!! Você foi aprovado")
+}else {
+    console.log(" Você foi reprovado sinto muito")
+}
