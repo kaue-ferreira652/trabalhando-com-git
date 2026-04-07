282292737f8ccaf2572ca4a1a81f310c59cf8e6c
kaue-ferreira652 <10kaue981@gmail.com>
2026-04-07 16:02:54 -0300

    subindo ex03.js

---
+++ ex03.js
@@ -0,0 +1,11 @@
+// const readline = require('readline-sync')
+
+// let nickname = readline.question('Informe seu nickname: ')
+// let jogoFavorito = readline.question('Seu jogo favorito: ')
+// let pontosAtual = readline.questionInt('Seus pontos: ')
+
+// console.log('====== PERFIL DE JOGADOR ======')
+// console.log(`Nickname: ${nickname}`)
+// console.log(`Jogo favorito: ${jogoFavorito}`)
+// console.log(`Pontos atuais: ${pontosAtual}`)
+
