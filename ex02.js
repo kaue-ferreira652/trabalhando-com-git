6a83f95f4aa10e8d4142c9bed0d9bf887fbc29da
kaue-ferreira652 <10kaue981@gmail.com>
2026-04-07 16:01:59 -0300

    subindo ex02.js

---
+++ ex02.js
@@ -0,0 +1,17 @@
+// const readline = require('readline-sync')
+
+// let nome = readline.question("Informe seu nome: ")
+// let nomeDoPrato = readline.question("Informe o nome do prato: ")
+// let valorDoPrato = readline.questionFloat("Informe o valor do prato: ")
+
+// console.log("========== RESUMO DO PEDIDO ==========")
+// console.log(`Cliente: ${nome}`)
+// console.log(`Prato escolhido: ${nomeDoPrato}`)
+// console.log(`Valor: ${valorDoPrato}`)
+
+const readline = require('readline-sync')
+
+let nickname = readline.question('Informe seu nickname: ')
+let jogoFavorito = readline.question('Seu jogo favorito: ')
+let pontuaçãoAtual = readline.questionInt('Seus pontos: ')
+
